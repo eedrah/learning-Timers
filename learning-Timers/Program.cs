4e8635543011a01f9caa81623f86463a08e5eb2d
Eedrah <eedrah@users.noreply.github.com>
2015-05-13 18:04:44 +1200

    write basic looping timer

--- learning-Timers/Program.cs
+++ learning-Timers/Program.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace learning_Timers {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            int numberRemaining = 5;
+            Timer timer = new Timer(500);
+
+            ElapsedEventHandler loopedEvent = (x, y) =>
+            {
+                Console.WriteLine(numberRemaining);
+
+                numberRemaining--;
+                if (numberRemaining <= 0)
+                {
+                    timer.Stop();
+                }
+            };
+            timer.Elapsed += loopedEvent;
+            timer.Start();
+
+            Console.ReadKey();
         }
     }
 }
